af6feed55e2c0bf12bd6d0ef08569af9fdee922c
BikramBiswas786 <106413198+BikramBiswas786@users.noreply.github.com>
2026-01-10 23:29:57 +0530

    Fix API handler to properly wrap Express app for Vercel serverless

--- api/index.js
+++ api/index.js
@@ -1,3 +1,9 @@
 import app from '../api-server/server.js';
 
-export default app;
+export default function handler(req, res) {
+  return new Promise((resolve, reject) => {
+    app(req, res);
+    res.on('finish', resolve);
+    res.on('error', reject);
+  });
+}
