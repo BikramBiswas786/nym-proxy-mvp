9959229009502b7b6efe325931f938720e44486e
BikramBiswas786 <106413198+BikramBiswas786@users.noreply.github.com>
2026-01-10 23:31:59 +0530

    Simplify API export - use Express app directly for Vercel routing

--- api/index.js
+++ api/index.js
@@ -1,9 +1,3 @@
 import app from '../api-server/server.js';
 
-export default function handler(req, res) {
-  return new Promise((resolve, reject) => {
-    app(req, res);
-    res.on('finish', resolve);
-    res.on('error', reject);
-  });
-}
+export default app;
